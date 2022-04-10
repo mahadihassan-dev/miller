000d576f845e5ece3cf9cb11bf92f0c72c8bb1b6
Mahedi Hassan <81403638+mahadihassan-dev@users.noreply.github.com>
2022-04-11 00:02:05 +0600

    New Section and Slider Update

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// JQuery Install
+$(document).ready(function() {
+    // Banner Slider
+    $('.banner_wrapper').slick({
+        dots: false,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 4099,
+    });
+});
